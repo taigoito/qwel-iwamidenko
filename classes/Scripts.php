9036bcdfd16402af5f141a4deb658dcfc1331459
Taigo Ito <webmaster@qwel.design>
2024-05-10 17:56:53 +0900

    Style the front page

--- classes/Scripts.php
+++ classes/Scripts.php
@@ -8,7 +8,7 @@ trait Scripts {
 		$version = wp_get_theme()->get( 'Version' );
 
 		// Google Fonts
-    $family = 'Noto+Sans+JP:wght@400;700&family=Poppins:ital,wght@0,200;0,400;0,700;1,200;1,400;1,700';
+    $family = 'Noto+Sans+JP:wght@400;700&family=Noto+Serif+JP&family=Poppins:ital,wght@0,200;0,400;0,700;1,200;1,400;1,700';
     if ($family !== '') {
       wp_enqueue_style('fonts', 'https://fonts.googleapis.com/css2?family=' . $family . '&display=swap', [], null); 
     }
