19eb3de2cc11aa624e62ac5863b9a58c1285e184
Taigo Ito <webmaster@qwel.design>
2024-05-28 19:35:26 +0900

    Correction instructions
    ・ページ名が画像とかぶり見づらい件ですが、文字に白枠をつけて頂きたいです。
    　お渡しした画像が見づらく申し訳ありませんがお願い致します。
    
    ・spのヘッダーのロゴサイズをもう少し小さくしてください。
    
    ・About Usページのspのconcept文章の行間を開けてください。
    　添付ファイルに修正箇所を記載しております。
    　Topページ内に以上の修正を依頼しておりましたが私のミスでした。
    　混乱させてしまい申し訳ありません。
    
    ・philosophyの黒い線をスクリーンショット致しました。ご確認のほどお願い致します。
    
    ・philosophyタイトル英字下「経営理念」を小さくしていただきたいです。
    　Top画像上部にある「私たちについて」と同じ大きさでお願い致します。
    
    ・前回Newsのprevをbackに変更していただきましたが、prev/nextが対になることを知りませんでした。
    　せっかく調整していただきましたがprevに戻していただきたいです。

--- js/_formatPagination.js
+++ js/_formatPagination.js
@@ -10,13 +10,13 @@ export default class FormatPagination {
     const prev = document.querySelector('.wp-block-query-pagination-previous');
     const next = document.querySelector('.wp-block-query-pagination-next');
 
-    if (prev) prev.textContent = '◀ back';
+    if (prev) prev.textContent = '◀ prev';
     if (next) next.textContent = 'next ▶';
 
     const myPrev = document.querySelector('.wp-block-query-my-pagination-previous a');
     const myNext = document.querySelector('.wp-block-query-my-pagination-next a');
 
-    if (myPrev) myPrev.textContent = '◀ back';
+    if (myPrev) myPrev.textContent = '◀ prev';
     if (myNext) myNext.textContent = 'next ▶';
   }
 }
